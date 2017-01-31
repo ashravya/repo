d78cbf162274111a07c1df2f04316161e54b1b8e
ShravyaAnchuru <shravya.anchuru@gmail.com>
2017-01-31 15:13:02 +0530

    rem

--- backup/UnitTesting/passwordvalidator/src/main/java/com/epam/model/Status.java
+++
@@ -1,5 +0,0 @@
-package com.epam.model;
-
-public enum Status {
-	PASS,FAIL
-}
--- backup/UnitTesting/passwordvalidator/src/main/java/com/epam/validators/Validator.java
+++
@@ -1,9 +0,0 @@
-package com.epam.validators;
-
-import java.util.List;
-
-import com.epam.model.Error;
-
-public interface Validator<T> {
-	public void validate(T input, List<Error> errors);
-}
